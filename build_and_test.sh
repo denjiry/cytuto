b058ea08574a7ecb3d79b2cbd139ca0c579d5cfc
kento-tajiri <k.denjiry@gmail.com>
2017-09-06 17:11:29 +0900

    also float64 work

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python setup.py build_ext --inplace
 #python -c "import ndtest;import numpy;a=ndtest.MyClass();print(a.run(numpy.arange(9).reshape((3,3)) ) )"
-python -c "import ndtest;import numpy;print(ndtest.run(numpy.arange(9,dtype=numpy.double).reshape((3,3)) ) )"
+python -c "import ndtest;import numpy;print(ndtest.run(numpy.arange(9,dtype=numpy.float64).reshape((3,3)) ) )"
